682c61ee0d919b8ed2c8c3afacdee9609c541d69
mallickhiba <mallickhiba@gmail.com>
2024-02-16 03:29:11 +0500

    Update user.js

--- models/User.js
+++ models/User.js
@@ -5,12 +5,11 @@ const UserSchema = new mongoose.Schema({
     password: String,
     firstName: String,
     lastName: String,
-    //role: {type: String,enum: ['Super', 'Admin', 'Poject Manager', 'Member'],default: 'Member'},
     admin: { type: Boolean, default: false },
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
 });
 
 const Users = mongoose.model('Users', UserSchema);
